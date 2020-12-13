63d949f85fbea7570018e553cbe2728acc953fe5
Melaku Abegaz <melaku.abegaz@uwrf.edu>
2020-12-12 23:48:54 -0600

    updated

--- test.R
+++ test.R
@@ -3,4 +3,7 @@ get_yesterday<-function(){
 }
 get_yesterday
 library(usethis)
-
+#generate token
+edit_r_environ()
+#in the env type: GITHUB_PAT= '127e2c7ba46102bb79f7a6d8d71bf7e64b034ca1'
+use_github(protocol = 'https', auth_token=Sys.betenv("GITHUB_PAT"))
